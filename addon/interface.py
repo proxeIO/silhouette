6395f48d1daa5b5acbc24f65c20e122c12ea86b5
trentinfrederick <trentin.shaun.frederick@gmail.com>
2016-12-10 15:33:35 -0600

    update

--- addon/interface.py
+++ addon/interface.py
@@ -1,8 +1,14 @@
 
 def toggle(self, context):
 
+  addon = context.user_preferences.addons[__name__.partition('.')[0]].preferences
+
   if context.space_data.viewport_shade == 'SOLID' or context.scene.silhouette.show_silhouette:
 
     layout = self.layout
 
     layout.prop(context.scene.silhouette, 'show_silhouette')
+
+    if context.scene.silhouette.show_silhouette:
+
+      layout.prop(addon, 'background_color', text='')
